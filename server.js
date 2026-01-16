1a6044eec82498a52b32bc26fc75ec0417ef72f5
codricageo <79510335+codricageo@users.noreply.github.com>
2026-01-16 22:20:24 +0200

    update server.js

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ const APP_VERSION = process.env.APP_VERSION || "dev";
 
 const menuItems = [
   { id: 1, name: "Margherita Pizza", price: 12.5, category: "Pizza" },
-  { id: 2, name: "BBQ Chicken Pizza", price: 14.0, category: "Bolognezeee" },
-  { id: 3, name: "Spaghetti Carbonara", price: 13.0, category: "Pasta" },
+  { id: 2, name: "BBQ Chicken Pizza", price: 11.0, category: "Bolognezeee" },
+  { id: 3, name: "Spaghetti Carbonara", price: 13.0, category: "Kjidha" },
   { id: 4, name: "Caesar Salad", price: 9.5, category: "Salad" },
-  { id: 5, name: "Tiramisu", price: 7.0, category: "Dessert" },
+  { id: 5, name: "Tiramisu", price: 7.0, category: "jsdgad" },
 ];
 
 app.use(express.json());
